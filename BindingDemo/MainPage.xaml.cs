77e48c021a5a43a2dafbb069a51f1aa0719cc973
Mauricio Andrés Contreras Contreras <m_contreras@outlook.cl>
2015-09-16 06:56:44 -0300

    Initial Commit
    Creación repo con demo funcionando de binding básico.

---
+++ BindingDemo/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace BindingDemo
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void btn_Sumar_Click(object sender, RoutedEventArgs e)
+        {
+            int resultado = int.Parse(tb_valor1.Text) + int.Parse(tb_valor2.Text);
+
+            tb_Resultado.Text = resultado.ToString();
+        }
+    }
+}
